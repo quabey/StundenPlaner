1d5cda530010cb9d127fa8ed37804ef517f30bdf
quabey <quadrobtv@gmail.com>
2022-09-27 14:01:11 +0200

    color update

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,13 +5,11 @@ module.exports = {
     extend: {
       colors : {
         'primary' : '#17223B',
-        'secondary-dark' : '#74a8b3',
         'secondary' : '#263859',
-        'primary-dark' : '#6a6ca8',
         'background' : '#6B778D',
         'special' : '#FF6768',
         'text' : '#E9E9E9',
-      }
+      },
     },
     
   },
